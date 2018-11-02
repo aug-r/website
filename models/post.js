cabed4a34bb8425b550fc01d2f06958fa1eb5977
Joey Pinhas <jpinhas97@yahoo.com>
2018-11-01 20:28:12 -0400

    Update retrieving posts to be more generic, and allow for more dynamic queries

--- models/post.js
+++ models/post.js
@@ -5,7 +5,17 @@ const connectObj = {
   useNewUrlParser: true,
 }
 
-function getOnePost(req, res, next) {
+/*
+  function: getPosts
+  This is a generic middleware function to get n posts, where n is set in
+  res.numberOfPosts. This abstraction allows calling functions to simply set 
+  the desired limit and call this function, keeping all fetch logic in one
+  place.
+*/
+function getPosts(req, res, next) {
+  const query = res.query || {};
+  const options = res.filters || {};
+  options.projection = { _id: 0 };
   const client = MongoClient(connectURL, connectObj);
   client.connect()
   .then((cl) => {
@@ -13,16 +23,56 @@ function getOnePost(req, res, next) {
     return db.collection('posts');
   })
   .then(posts => {
-    return posts.findOne({}, { projection: { _id: 0 }});
+    return posts.find(query, options).toArray();
   })
   .then(result => {
-    res.data = result;
+    res.data = result.length == 1 ? result[0] : result;
     client.close();
     next();
   })
   .catch(err => next(err));
 }
 
+function getOnePost(req, res, next) {
+  res.query = {};
+  res.filters = {
+    limit: 1,
+  };
+  next();
+}
+
+function getMostRecentPosts(req, res, next) {
+  // get the number of posts to fetch from the req, defaulting to 10
+  const number = req.params.number_of_posts || req.body.number_of_posts || 10;
+  res.query = {};
+  res.filters = {
+    limit: paresInt(number, 10),
+    sort: {
+      date: -1,
+    }
+  }
+  next();
+}
+
+function getPostsByTitle(req, res, next) {
+  const title = req.params.post_title || req.body.post_title;
+  if (!title) {
+    return next(new Error('Must specify a title'));
+  }
+
+  res.query = {
+    title: title,
+  }
+  res.filters = {
+    sort: {
+      date: -1,
+    }
+  }
+}
+
 module.exports = {
-  getOnePost
+  getPosts,
+  getOnePost,
+  getMostRecentPosts,
+  getPostsByTitle,
 };
--- routes/posts.js
+++ routes/posts.js
@@ -7,7 +7,13 @@ function sendAsJSON(req, res) {
   res.json(res.data);
 }
 
-router.route('/')
-  .get(postModel.getOnePost, sendAsJSON);
+router.route('/one')
+  .get(postModel.getOnePost, postModel.getPosts, sendAsJSON);
+
+router.route('/recent')
+  .get(postModel.getMostRecentPosts, postModel.getPosts, sendAsJSON);
+
+router.route('/search')
+  .get(postModel.getPostsByTitle, postModel.getPosts, sendAsJSON);
 
 module.exports = router;
